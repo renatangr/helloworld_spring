fe6f377bd87404ee73d86c63aaf8b6b15870d75d
Renata Negrini <renatanegrinim@gmail.com>
2025-05-15 12:20:51 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+	@GetMapping
+	public String hello() {
+		return "<b>Hello World!</b>";
+	}
+	
+	@GetMapping("/turma82")
+	public String turmaJava82() {
+		return "<marquee>hello world, turma java 82!</marquee>";
+	}
+}
